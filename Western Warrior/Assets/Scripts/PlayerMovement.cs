99061b2a7003db7c3fbb464340c9e6d2d4f44e9a
Jeremy Martin <jeremymartin4794@gmail.com>
2022-10-12 23:26:03 -0400

    adding movement scripts

---
+++ Western Warrior/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float movementSpeed = 5;
+    private Vector2 rotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey("d"))
+        {
+            Debug.Log("test");
+            transform.Translate(new Vector2(1, 0) * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            Debug.Log("test");
+            transform.Translate(new Vector2(-1, 0) * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            Debug.Log("test");
+            transform.Translate(new Vector2(0, 1) * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            Debug.Log("test");
+            transform.Translate(new Vector2(0, -1) * movementSpeed * Time.deltaTime);
+        }
+    }
+}
